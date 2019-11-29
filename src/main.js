bede03e0a3a07c35bd3faad6420a320173bb464f
mk668a <msftoaa@gmail.com>
2019-11-29 19:45:22 +0900

    [main.js] add firebase

--- src/main.js
+++ src/main.js
@@ -5,6 +5,24 @@ import store from './store'
 import './registerServiceWorker'
 require('aframe')
 
+// firebase
+import firebase from 'firebase'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyCRwBG7hWpMeXyH1O28wmcx__JZw4ZEQ-U",
+    authDomain: "webar-8fa80.firebaseapp.com",
+    databaseURL: "https://webar-8fa80.firebaseio.com",
+    projectId: "webar-8fa80",
+    storageBucket: "webar-8fa80.appspot.com",
+    messagingSenderId: "996519997485",
+    appId: "1:996519997485:web:c41496eabb41003df088a4"
+  })
+}
+
+export default firebase
+// end of firebase
+
 Vue.config.productionTip = false
 
 new Vue({
@@ -12,3 +30,4 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
